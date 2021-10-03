3ee8999e809a3bcab99b8ff8446bd23337c444f8
cristian980829 <cristian980829@gmail.com>
2021-10-03 18:10:16 -0500

    Basci project creation

---
+++ PetCare.Api/Data/Entities/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetCare.Api.Data.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DocumentType> DocumentTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<DocumentType>().HasIndex(x => x.Description).IsUnique();
+        }
+    
+    }
+}
