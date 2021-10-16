170e4be4e5b7c3f954af3d931968f6d05e01eb13
Camilo Jaramillo <camilojd77@gmail.com>
2021-10-16 17:03:58 -0500

    Task CheckProceduresAsync was updated.

--- PetCare.Api/Data/SeedDb.cs
+++ PetCare.Api/Data/SeedDb.cs
@@ -35,31 +35,9 @@ namespace PetCare.Api.Data
             if (!_context.Procedures.Any())
             {
                 _context.Procedures.Add(new Procedure { Price = 10000, Description = "Extracción de sangre" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Lubricación de suspención delantera" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Lubricación de suspención trasera" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Frenos delanteros" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Frenos traseros" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Líquido frenos delanteros" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Líquido frenos traseros" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Calibración de válvulas" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Alineación carburador" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Aceite motor" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Aceite caja" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Filtro de aire" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Sistema eléctrico" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Guayas" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Cambio llanta delantera" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Cambio llanta trasera" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Reparación de motor" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Kit arrastre" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Banda transmisión" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Cambio batería" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Lavado sistema de inyección" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Lavada de tanque" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Cambio de bujia" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Cambio rodamiento delantero" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Cambio rodamiento trasero" });
-                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Accesorios" });
+                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Profilaxis" });
+                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Corte de pelo" });
+                _context.Procedures.Add(new Procedure { Price = 10000, Description = "Revisión general" });
                 await _context.SaveChangesAsync();
             }
         }
